eb09470a57ab639fe30f307792f4873199b25feb
Dat Duong <duongquangdat172004@gmail.com>
2023-10-05 16:44:31 +0700

    them file admin va staff

---
+++ QuanLiCoffeeShop/View/Staff/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCoffeeShop.View.Staff
+{
+    internal class Class1
+    {
+    }
+}

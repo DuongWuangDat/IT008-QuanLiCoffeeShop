9b49e33e5f8620a7d18ae084438604a52b0222e0
Dat Duong <duongquangdat172004@gmail.com>
2023-10-12 11:51:48 +0700

    create viewmodel resource

---
+++ QuanLiCoffeeShop/ViewModel/LoginVM/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using QuanLiCoffeeShop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCoffeeShop.ViewModel.LoginVM
+{
+    public class LoginViewModel : BaseViewModel
+    {
+    }
+}

bbaa2b0c1d532adaa3ce3d062d996beb73cd41c1
Dat Duong <duongquangdat172004@gmail.com>
2023-10-13 09:17:32 +0700

    fix database

--- QuanLiCoffeeShop/Model/Model1.Context.cs
+++ QuanLiCoffeeShop/Model/Model1.Context.cs
@@ -34,6 +34,5 @@ namespace QuanLiCoffeeShop.Model
         public virtual DbSet<Product> Product { get; set; }
         public virtual DbSet<Seat> Seat { get; set; }
         public virtual DbSet<Staff> Staff { get; set; }
-        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
     }
 }

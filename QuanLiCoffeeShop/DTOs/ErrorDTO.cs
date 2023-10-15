8eafcfeac877d432f64032814506b239ec2a23e3
Dat Duong <duongquangdat172004@gmail.com>
2023-10-15 11:22:46 +0700

    add data transfer object (DTO)

---
+++ QuanLiCoffeeShop/DTOs/ErrorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCoffeeShop.DTOs
+{
+    public class ErrorDTO
+    {
+        public int ID { get; set; }
+        public string DisplayName { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+    }
+}

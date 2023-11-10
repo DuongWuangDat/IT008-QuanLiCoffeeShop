7ee95ce9404060dafadfa4cb4e396814efd75839
Nguyen Duong <nduongg04@gmail.com>
2023-11-10 21:39:08 +0700

    Update Staff

--- QuanLiCoffeeShop/Model/Service/StaffService.cs
+++ QuanLiCoffeeShop/Model/Service/StaffService.cs
@@ -29,7 +29,7 @@ namespace QuanLiCoffeeShop.Model.Service
 				return _ins;
 			}
 			private set { _ins = value; }
-		}
+		}// test
 
 		//Get all staff
 		public async Task<List<StaffDTO>> GetAllStaff()

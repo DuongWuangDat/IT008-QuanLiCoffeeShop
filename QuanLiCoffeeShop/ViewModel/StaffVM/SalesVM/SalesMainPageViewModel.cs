66cb25200e6883798782cf05ff79e6e3efc83b9c
Tran Thanh Dat <neban0444@gmail.com>
2023-11-17 18:15:42 +0700

    p Sales I of StaffPage

---
+++ QuanLiCoffeeShop/ViewModel/StaffVM/SalesVM/SalesMainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using QuanLiCoffeeShop.DTOs;
+using QuanLiCoffeeShop.Model;
+using QuanLiCoffeeShop.Model.Service;
+using QuanLiCoffeeShop.Utils;
+using QuanLiCoffeeShop.View.Staff.Sales;
+using QuanLiCoffeeShop.View.MessageBox;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace QuanLiCoffeeShop.ViewModel.StaffVM.SalesVM
+{
+    internal class SalesMainPageViewModel:BaseViewModel
+    {
+        public ICommand LoadSeatPageCM {  get; set; }
+        public ICommand LoadProductPageCM { get; set; }
+        public ICommand FirstLoadCM { get; set; }
+        public SalesMainPageViewModel() {
+            FirstLoadCM = new RelayCommand<Frame>((p) => { return true; }, (p) => {
+                p.Content = new SeatPage();
+            });
+            LoadSeatPageCM = new RelayCommand<Frame>((p)=> { return true; }, (p)=> {
+                p.Content = new SeatPage();
+            });
+            LoadProductPageCM = new RelayCommand<Frame>((p) => { return true; }, (p) => {
+                p.Content = new ProductPage();
+            });
+        }
+    }
+}

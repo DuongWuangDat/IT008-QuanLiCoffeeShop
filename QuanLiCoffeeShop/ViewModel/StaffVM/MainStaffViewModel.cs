e8528029e0010e04eaba421a5ad9351a3e367d77
Tran Thanh Dat <neban0444@gmail.com>
2023-11-18 15:37:43 +0700

    add

--- QuanLiCoffeeShop/ViewModel/StaffVM/MainStaffViewModel.cs
+++ QuanLiCoffeeShop/ViewModel/StaffVM/MainStaffViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using QuanLiCoffeeShop.DTOs;
+using QuanLiCoffeeShop.View.Staff.Sales;
 using QuanLiCoffeeShop.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLiCoffeeShop.ViewModel.StaffVM
 {
     public class MainStaffViewModel : BaseViewModel
     {
         public static StaffDTO curentstaff;
-        public MainStaffViewModel() { }
+        private string _currentName;
+
+        public string currentName
+        {
+            get { return _currentName; }
+            set { _currentName = value; OnPropertyChanged(); }
+        }
+        public ICommand FirstLoadCM {  get; set; }
+        
+        public MainStaffViewModel() {
+            FirstLoadCM = new RelayCommand<Frame>((p) => { return true; }, (p) => {
+                p.Content = new SalesMainPage();
+                currentName = curentstaff.DisplayName;
+            });
+        }
     }
 }
